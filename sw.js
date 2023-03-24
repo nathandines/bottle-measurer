aca4277020425953a828bd5b89b0d2a729aea994
Nathan Dines <nathandines@users.noreply.github.com>
2023-03-25 08:27:08 +1100

    Improve service worker

--- sw.js
+++ sw.js
@@ -1,10 +1,10 @@
 const cacheName = "v1";
 const contentToCache = [
-    "/index.html",
-    "/app.js",
-    "/app.webmanifest",
-    "/assets/bootstrap@5.3.0-alpha2/bootstrap.bundle.min.js",
-    "/assets/bootstrap@5.3.0-alpha2/bootstrap.min.css"
+    "index.html",
+    "app.js",
+    "app.webmanifest",
+    "assets/bootstrap@5.3.0-alpha2/bootstrap.bundle.min.js",
+    "assets/bootstrap@5.3.0-alpha2/bootstrap.min.css"
 ];
 
 self.addEventListener("install", (e) => {
@@ -15,5 +15,18 @@ self.addEventListener("install", (e) => {
             console.log("[Service Worker] Caching content");
             await cache.addAll(contentToCache);
         })()
+    );
+});
+
+self.addEventListener("activate", (e) => {
+    e.waitUntil(
+        caches.keys().then((keyList) => {
+            return Promise.all(
+                keyList.map((key) => {
+                    if (key === cacheName) return;
+                    return caches.delete(key);
+                })
+            );
+        })
     );
 });
