e6f4090ea9a5ffe919ec3ac1b204da9c0a89c8a2
Nathan Dines <nathandines@users.noreply.github.com>
2023-03-25 07:54:43 +1100

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Bottle Measurer Site
+
+This is a simple website so my wife and I can easily see how much formula is
+remaining in our child's bottles. The bottle markings are innacurate, so using
+weight is a better option.
---
+++ measure.js
@@ -0,0 +1,21 @@
+const weightInput = document.getElementById("weight-input");
+const bottleTypeSelector = document.getElementById("bottle-type-selector");
+const resultField = document.getElementById("result");
+
+const bottleWeights = {
+    'pigeon-small': 33.3,
+    'pigeon-large': 43.1
+}
+const hundredMlFormulaWeight = 103
+
+weightInput.addEventListener("input", updateResult);
+bottleTypeSelector.addEventListener("input", updateResult);
+
+function updateResult() {
+    const baseBottleWeight = bottleWeights[bottleTypeSelector.value]
+    const totalBottleWeight = weightInput.value
+
+    const mlRemaining = Math.ceil((totalBottleWeight - baseBottleWeight) / (hundredMlFormulaWeight / 100))
+
+    resultField.innerHTML = isNaN(mlRemaining) ? 0 : Math.max(mlRemaining, 0);
+}
